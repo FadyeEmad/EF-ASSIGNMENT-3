b00bdb88afaa4977cb577dde8bc94ff477277a27
fady emad <femad7732@gmail.con>
2024-10-31 17:17:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿#region  Join Operators : Join
+using System;
+
+using AppDbContext context = new AppDbContext();
+var Result = context.Employees.Join(context.Departments,
+E => E.DeptId,
+    D => D.Id,
+    (E, D) => new
+    {
+        EmpId = E.Id,
+        EmpName = E.Name,
+        DeptId = D.Id,
+        DeptName = D.Name
+    });
+
+foreach (var item in Result)
+{
+    Console.WriteLine(item);
+}
+
+#endregion
+
+#region Tracking Vs NoTracking
+// Tracking [Default]
+
+using AppDbContext context = new AppDbContext();
+
+var employee = context.Employees.AsNoTracking().FirstOrDefault(E => E.Id == 2);
+
+Console.WriteLine(context.Entry(employee).State); // Detached
+
+employee.Name = "Ali"; // Local
+
+Console.WriteLine(context.Entry(employee).State); // Local
+
+context.SaveChanges(); // Remotely
+
+// Console.WriteLine(employee.Name);
+
+
+#endregion
+
+#region Local Vs Remote
+
+using AppDbContext context = new AppDbContext();
+
+context.Employees.Load();
+
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+context.Employees.Local.Any();
+
+
+#endregion
---
+++ migration/20240910160743_create 2.cs
@@ -0,0 +1,320 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_SESSION_1_ASSIGNMENT.Migrations
+{
+    public partial class create2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_stud_Courses",
+                table: "stud_Courses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Course_inst",
+                table: "Course_inst");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "topic",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lname",
+                table: "Students",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fname",
+                table: "Students",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Dep_id",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Students",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Students",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Instructors",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Instructors",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ins_id",
+                table: "Departments",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "hiringdate",
+                table: "Departments",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "varchar",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "courses",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "courses",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "evaluate",
+                table: "Course_inst",
+                type: "string",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "inst_id",
+                table: "Course_inst",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Coure_id",
+                table: "Course_inst",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Course_inst",
+                table: "Course_inst",
+                column: "inst_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Course_inst",
+                table: "Course_inst");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "topic",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lname",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fname",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Dep_id",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "Instructors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Instructors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ins_id",
+                table: "Departments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "hiringdate",
+                table: "Departments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "evaluate",
+                table: "Course_inst",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "string");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Coure_id",
+                table: "Course_inst",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "inst_id",
+                table: "Course_inst",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_stud_Courses",
+                table: "stud_Courses",
+                columns: new[] { "Course_id", "stud_id" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Course_inst",
+                table: "Course_inst",
+                columns: new[] { "Coure_id", "inst_id" });
+        }
+    }
+}
